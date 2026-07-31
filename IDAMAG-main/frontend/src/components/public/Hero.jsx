4920696eabda026876a968f5cfbffe727278a15b
janvic0304 <janvic0304@gmail.com>
2026-07-31 11:30:24 +0800

    lunchba

--- IDAMAG-main/frontend/src/components/public/Hero.jsx
+++ IDAMAG-main/frontend/src/components/public/Hero.jsx
@@ -6,20 +6,23 @@ const Hero = () => {
       className="
         relative
         w-full
+        h-[calc(100dvh-104px)]
+        min-h-[400px]
         overflow-hidden
         bg-white
       "
     >
-      {/* HERO IMAGE */}
+      {/* HERO BACKGROUND */}
       <img
         src="/i-damag.png"
         alt="I-DAMAG Background"
         className="
-          block
+          absolute
+          inset-0
           w-full
-          h-auto
-          max-w-none
+          h-full
           object-contain
+          object-center
           select-none
           pointer-events-none
         "
@@ -36,76 +39,72 @@ const Hero = () => {
           pointer-events-none
         "
       >
-        <div
+        <a
+          href="#links"
           className="
-            translate-y-[18%]
             pointer-events-auto
+
+            inline-flex
+            items-center
+            justify-center
+            gap-2
+            md:gap-3
+
+            translate-y-[18vh]
+
+            bg-[#106837]
+            hover:bg-[#0d542c]
+
+            text-white
+            font-bold
+
+            text-sm
+            sm:text-base
+            md:text-xl
+
+            px-4
+            py-2
+
+            sm:px-6
+            sm:py-3
+
+            md:px-8
+            md:py-4
+
+            lg:px-10
+            lg:py-5
+
+            rounded-2xl
+
+            shadow-2xl
+
+            transition-all
+            duration-300
+
+            hover:scale-105
+            hover:shadow-[#106837]/40
           "
         >
-          <a
-            href="#links"
+          Explore Divisions
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
             className="
-              inline-flex
-              items-center
-              justify-center
-
-              gap-2
-              md:gap-3
-
-              bg-[#106837]
-              hover:bg-[#0d542c]
-
-              text-white
-              font-bold
-
-              text-sm
-              sm:text-base
-              md:text-xl
-
-              px-4
-              py-2
-
-              sm:px-6
-              sm:py-3
-
-              md:px-8
-              md:py-4
-
-              lg:px-10
-              lg:py-5
-
-              rounded-2xl
-
-              transition-all
-              duration-300
-
-              hover:scale-105
-
-              shadow-2xl
-              hover:shadow-[#106837]/40
+              w-4
+              h-4
+              md:w-6
+              md:h-6
             "
+            viewBox="0 0 20 20"
+            fill="currentColor"
           >
-            Explore Divisions
-
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="
-                w-4
-                h-4
-                md:w-6
-                md:h-6
-              "
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M16.707 10.293a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l4.293-4.293a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>
-        </div>
+            <path
+              fillRule="evenodd"
+              d="M16.707 10.293a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l4.293-4.293a1 1 0 011.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </a>
       </div>
     </section>
   );
